36d072e3f8c35442da4b4fa3e3d1488b53f26aa9
Raymond R-Mcnaught <raymond.richards-mcnaught@grey.com>
2016-07-02 18:14:34 -0400

    -Parsed JSON to HMTL using Ajax Request

--- js/jsonBuild.js
+++ js/jsonBuild.js
@@ -1,28 +1,69 @@
-$(function(){
-	
-	'use strict'
+'use strict'
 
-	var jsonFile = 'quiz.json';
-	console.log('WORKING');
+/*
+ * Either assign object to already 
+ established object or start over!!!
+ */
+var jasonBuild = window.jasonBuild || {};
 
-	$.getJSON(jsonFile, function(quiz){
 
-		console.log(quiz.question1,' ',quiz.answer1[0]);
+//Object
+var jasonBuild = {
 
-		var output = '';
+	jsonFile: "data/info.txt",
+	index: null,
+	infoList: null,
+	outputDiv: null,
+	jsonArr: null,
 
-		for ( key in quiz.answer1) {
-			if( quiz.answer1.hasOwnProperty(key) ){
-				output+= '<li>' + quiz.answer1[key] + '</li>';
-				console.log(output);
-			}
+	init: function(){
+		jasonBuild.index = 0;
+		jasonBuild.infoList = document.querySelector('#info-list');
+		jasonBuild.outputDiv = document.querySelector('#output-div');
+		jasonBuild.jsonArr = [];
+		jasonBuild.run();//run
+		console.log('INITIALIZE');
+	},
+
+	loadJson: function(file, callBack){
+		// XHR Request for server data
+		// Assing quiz.data to XHR response
+		// Intead simple parse
+		//return(JSON.parse(file);
+		var xmlhttp = new XMLHttpRequest();
+		var url = file;
+
+		xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			    jasonBuild.jsonArr = JSON.parse(xmlhttp.responseText);
+			    jasonBuild.styleJSON(jasonBuild.jsonArr);//
+			    console.log('JSON FILE ',jasonBuild.jsonArr);
+			    //return jasonBuild.jsonLoaded;
+		    }
 		};
 
-		var response = document.getElementById('answers');
-		answers.innerHTML = output;
-	}).done(function(quiz){
-		console.log('SUCCESS!!!!');
-	});// getJSON
+		xmlhttp.open("GET", url, true);
+		xmlhttp.send();
+	},
 
+	styleJSON: function(arr){
+		var out = "";
+	    for(var i = 0; i < jasonBuild.jsonArr.length; i++) {
+	        out += '<a href="' + jasonBuild.jsonArr[i].url + '">' + 
+	        jasonBuild.jsonArr[i].display + '</a><br>';
+	    }
+	    jasonBuild.outputDiv.innerHTML = out;
+	    console.log('Output ',out, ' jsonArr ',jasonBuild.jsonArr);
+	},
 
+	run: function(arr){
+		jasonBuild.loadJson(jasonBuild.jsonFile);// load json file
+	}
+
+};
+
+//When contet is loaded in the Dom run
+document.addEventListener('DOMContentLoaded', function(){
+	jasonBuild.init();
 });
+
